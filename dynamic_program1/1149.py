959defc3bc9a6b8a2d546a7f33e7383d72909b4c
jongwon <whd7327@naver.com>
2022-10-03 11:41:25 +0900

    bak

--- dynamic_program1/1149.py
+++ dynamic_program1/1149.py
@@ -2,6 +2,8 @@ n = int(input())
 p = []
 for i in range(n):
     p.append(list(map(int, input().split())))
+
+print(p)
 for i in range(1, len(p)):
     p[i][0] = min(p[i - 1][1], p[i - 1][2]) + p[i][0]
     p[i][1] = min(p[i - 1][0], p[i - 1][2]) + p[i][1]
